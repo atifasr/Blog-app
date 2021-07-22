43828c01fa5db1e5977876c3f44d7d56d1dacc92
atif <atifshafi63@gmail.com>
2021-07-22 10:56:46 +0530

    made some basic changes

--- server_log/settings.py
+++ server_log/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from posixpath import basename
+from .secret import key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8-&y(yj_+b6t9+^la^0dcy5n3i^3#4njc8vpzel3l(h(z%6her'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
